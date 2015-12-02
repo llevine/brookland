f9bfdd6046a972d6da3bc5d1fb86dd0899d1b96a
llevine <laurenlevinedesigns@gmail.com>
2015-12-02 14:57:18 -0500

    reworded the contact blurb, italicized the book title, bolded callouts in the white bubble container, added social media to mobile, removed contact_mobile from dom, move #contact using js on mobile version, changed helvetica to source sans pro, put the second sentence of white bubble in a span so it can be removed on mobile version per pdf

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 	if ($(".dot_divider").css("visibility") == "hidden" ){
        $('#white_circle_container').insertBefore('#book_cover');
        $('#color_divider').appendTo('#main_content');
-       $('#contact_mobile').insertBefore('#color_divider');
+       $('#social_media_container').insertAfter('#header');
+       $('#contact').insertBefore('#color_divider');
        $('.dot_divider').css({"visibility": "hidden", "display": "block"});
        // alert("mobile");    
     }
  	else {
  		// alert('desktop');
  	}
-});
+}); 
